38c61e883f877cf56cd788ace3a7077f4c699159
Silvia <silvia8gasparini@gmail.com>
2025-06-06 11:04:20 +0200

    generate project

---
+++ src/main/java/it/epicode/U5W1D5project/U5W1D5projectApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.U5W1D5project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D5projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D5projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/U5W1D5project/U5W1D5projectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U5W1D5project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D5projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
